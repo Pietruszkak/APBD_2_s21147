8a64ebdfd37019e40dc8218da9dcece4d5341c43
s21147 <s21147@pjwstk.edu.pl>
2024-03-08 13:11:26 +0100

    added feature-average

--- APBD_2/APBD_2/Program.cs
+++ APBD_2/APBD_2/Program.cs
@@ -13,6 +13,17 @@ namespace APBD_2
             Console.WriteLine("Hello");
             Console.WriteLine("Modyfikacja 2");
             Console.WriteLine("Modyfikacja 3");
+            Console.WriteLine(GetAvg([1, 2, 3, 4, 5]));
+
+
+            static double GetAvg(int[] arr) {
+                int sum=0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum=sum+arr[i];
+                }
+                return sum/arr.Length;
+            }
         }
     }
 }
