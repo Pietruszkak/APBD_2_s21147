bf5e8cd548ee20d80138c4a109b768becdfc6458
s21147 <s21147@pjwstk.edu.pl>
2024-03-08 13:25:29 +0100

    added feature-max

--- APBD_2/APBD_2/Program.cs
+++ APBD_2/APBD_2/Program.cs
@@ -14,6 +14,7 @@ namespace APBD_2
             Console.WriteLine("Modyfikacja 2");
             Console.WriteLine("Modyfikacja 3");
             Console.WriteLine(GetAvg([1, 2, 3, 4, 5]));
+            Console.WriteLine(GetMax([1, 333, 3, 214, 5]));
 
 
             static double GetAvg(int[] arr) {
@@ -23,6 +24,18 @@ namespace APBD_2
                     sum=sum+arr[i];
                 }
                 return sum/arr.Length;
+            }
+            static int GetMax(int[] arr)
+            {
+                int max = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if arr[i]> max)
+                    {
+                        max = arr[i];
+                    }
+                }
+                return max;
             }
         }
     }
